3ce878eb48bd8e8615a1aeace1bd7196f6bb978e
Yuvi <ykaushal364@gmail.com>
2022-05-14 19:12:08 +0530

    initial commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -73,10 +73,10 @@ const Navbar = () => {
     <AppBar position="fixed">
       <Toolbar className={classes.toolbar}>
         <Typography variant="h6" className={classes.logoLg}>
-          Lama Dev
+          Yuvraj
         </Typography>
         <Typography variant="h6" className={classes.logoSm}>
-          LAMA
+          Yuvi
         </Typography>
         <div className={classes.search}>
           <Search />
